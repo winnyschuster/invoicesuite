7efda6d159219ad574b53e36da5cce98e31e1b9d
HorstOeko <horstoeko@erling.com.de>
2025-11-07 05:53:53 +0100

    [ENH] More on InvoiceSuitePdfDocumentBuilderTest

--- tests/testcases/pdf/InvoiceSuitePdfDocumentBuilderTest.php
+++ tests/testcases/pdf/InvoiceSuitePdfDocumentBuilderTest.php
@@ -4,29 +4,27 @@ declare(strict_types=1);
 
 namespace horstoeko\invoicesuite\tests\testcases\pdf;
 
-use horstoeko\invoicesuite\InvoiceSuiteDocumentBuilder;
-use horstoeko\invoicesuite\InvoiceSuitePdfDocumentBuilder;
-use horstoeko\invoicesuite\pdfs\abstracts\InvoiceSuiteAbstractPdfConstructor;
-use horstoeko\invoicesuite\pdfs\zffx\InvoiceSuiteZffxPdfConstructor;
-use horstoeko\invoicesuite\pdfs\zffx\InvoiceSuiteZffxPdfWriter;
+use Closure;
+use DateTime;
 use horstoeko\invoicesuite\tests\TestCase;
+use horstoeko\invoicesuite\InvoiceSuiteDocumentBuilder;
 use horstoeko\invoicesuite\utils\InvoiceSuitePathUtils;
+use horstoeko\invoicesuite\InvoiceSuitePdfDocumentBuilder;
+use horstoeko\invoicesuite\pdfs\zffx\InvoiceSuiteZffxPdfWriter;
+use horstoeko\invoicesuite\pdfs\zffx\InvoiceSuiteZffxPdfConstructor;
+use horstoeko\invoicesuite\codelists\InvoiceSuiteCodelistDocumentTypes;
+use horstoeko\invoicesuite\pdfs\abstracts\InvoiceSuiteAbstractPdfConstructor;
 
 final class InvoiceSuitePdfDocumentBuilderTest extends TestCase
 {
-    private function getSampleXmlPath(): string
-    {
-        return InvoiceSuitePathUtils::combineAllPaths(__DIR__, "..", "..", "assets");
-    }
-
-    private function getSamplePdfPath(): string
+    private function getAssetPath(): string
     {
         return InvoiceSuitePathUtils::combineAllPaths(__DIR__, "..", "..", "assets");
     }
 
     private function getSamplePlainPdfFile(): string
     {
-        return InvoiceSuitePathUtils::combinePathWithFile($this->getSamplePdfPath(), "pdf_plain.pdf");
+        return InvoiceSuitePathUtils::combinePathWithFile($this->getAssetPath(), "pdf_plain.pdf");
     }
 
     public static function zffxProfileProvider(): iterable
@@ -66,7 +64,7 @@ final class InvoiceSuitePdfDocumentBuilderTest extends TestCase
     {
         if ($expectusePdfContent !== true) {
             if ($expectedUseOfXmlFile !== false) {
-                $xmlFilename = InvoiceSuitePathUtils::combinePathWithFile($this->getSampleXmlPath(), $expectedUseOfXmlFile);
+                $xmlFilename = InvoiceSuitePathUtils::combinePathWithFile($this->getAssetPath(), $expectedUseOfXmlFile);
                 $xmlContent = file_get_contents($xmlFilename);
                 $pdfDOcumentBuilder = InvoiceSuitePdfDocumentBuilder::createFromDocumentContentAndPdfFile(
                     $xmlContent,
@@ -75,6 +73,10 @@ final class InvoiceSuitePdfDocumentBuilderTest extends TestCase
             } else {
                 $documentBuilder = InvoiceSuiteDocumentBuilder::createByProviderUniqueId($expectedProfile);
                 $documentBuilder->setDocumentNo('2025-00000001');
+                $documentBuilder->setDocumentType(InvoiceSuiteCodelistDocumentTypes::COMMERCIAL_INVOICE->value);
+                $documentBuilder->setDocumentSellerId('0815');
+                $documentBuilder->setDocumentSellerName('Seller Name');
+                $documentBuilder->setDocumentDate(DateTime::createFromFormat('Ymd', '19700101'));
                 $pdfDOcumentBuilder = InvoiceSuitePdfDocumentBuilder::createFromDocumentBuilderAndPdfFile(
                     $documentBuilder,
                     $this->getSamplePlainPdfFile()
@@ -83,7 +85,7 @@ final class InvoiceSuitePdfDocumentBuilderTest extends TestCase
         } else {
             $pdfContent = file_get_contents($this->getSamplePlainPdfFile());
             if ($expectedUseOfXmlFile !== false) {
-                $xmlFilename = InvoiceSuitePathUtils::combinePathWithFile($this->getSampleXmlPath(), $expectedUseOfXmlFile);
+                $xmlFilename = InvoiceSuitePathUtils::combinePathWithFile($this->getAssetPath(), $expectedUseOfXmlFile);
                 $xmlContent = file_get_contents($xmlFilename);
                 $pdfDOcumentBuilder = InvoiceSuitePdfDocumentBuilder::createFromDocumentContentAndPdfContent(
                     $xmlContent,
@@ -92,6 +94,10 @@ final class InvoiceSuitePdfDocumentBuilderTest extends TestCase
             } else {
                 $documentBuilder = InvoiceSuiteDocumentBuilder::createByProviderUniqueId($expectedProfile);
                 $documentBuilder->setDocumentNo('2025-00000001');
+                $documentBuilder->setDocumentType(InvoiceSuiteCodelistDocumentTypes::COMMERCIAL_INVOICE->value);
+                $documentBuilder->setDocumentSellerId('0815');
+                $documentBuilder->setDocumentSellerName('Seller Name');
+                $documentBuilder->setDocumentDate(DateTime::createFromFormat('Ymd', '19700101'));
                 $pdfDOcumentBuilder = InvoiceSuitePdfDocumentBuilder::createFromDocumentBuilderAndPdfContent(
                     $documentBuilder,
                     $pdfContent
@@ -121,10 +127,10 @@ final class InvoiceSuitePdfDocumentBuilderTest extends TestCase
 
         // PdfWriter
 
-        $prop = $this->getPrivatePropertyFromObject($propValue, 'pdfWriter');
-        $propValue = $prop->getValue($propValue);
+        $propPdfWriter = $this->getPrivatePropertyFromObject($propValue, 'pdfWriter');
+        $propPdfWriterValue = $propPdfWriter->getValue($propValue);
 
-        $this->assertInstanceOf(InvoiceSuiteZffxPdfWriter::class, $propValue);
+        $this->assertInstanceOf(InvoiceSuiteZffxPdfWriter::class, $propPdfWriterValue);
 
         // Initial values
 
@@ -194,6 +200,51 @@ final class InvoiceSuitePdfDocumentBuilderTest extends TestCase
 
         $pdfDOcumentBuilder->setMetaInformationAuthorTemplate('Some author template');
         $this->assertSame("Some author template", $pdfDOcumentBuilder->getMetaInformationAuthorTemplate());
+        $pdfDOcumentBuilder->setMetaInformationKeywordTemplate('Some keyword template');
+        $this->assertSame("Some keyword template", $pdfDOcumentBuilder->getMetaInformationKeywordTemplate());
+        $pdfDOcumentBuilder->setMetaInformationTitleTemplate('Some title template');
+        $this->assertSame("Some title template", $pdfDOcumentBuilder->getMetaInformationTitleTemplate());
+        $pdfDOcumentBuilder->setMetaInformationSubjectTemplate('Some subject template');
+        $this->assertSame("Some subject template", $pdfDOcumentBuilder->getMetaInformationSubjectTemplate());
+        $pdfDOcumentBuilder->setMetaInformationCallback(function () {
+            return "Some Result";
+        });
+        $this->assertInstanceOf(Closure::class, $pdfDOcumentBuilder->getMetaInformationCallback());
+
+        $pdfDOcumentBuilder->setAttachmentPaneVisibility(false);
+        $this->assertFalse($pdfDOcumentBuilder->getAttachmentPaneVisibility());
+        $pdfDOcumentBuilder->setAttachmentPaneVisibility(true);
+        $this->assertTrue($pdfDOcumentBuilder->getAttachmentPaneVisibility());
+
+        $pdfDOcumentBuilder->addAdditionalDocumentByRealFile(InvoiceSuitePathUtils::combinePathWithFile($this->getAssetPath(), "02_picture.jpg"));
+        $additionalContent = file_get_contents(InvoiceSuitePathUtils::combinePathWithFile($this->getAssetPath(), "02_picture.jpg"));
+        $pdfDOcumentBuilder->addAdditionalDocumentByContent($additionalContent, 'pdf.pdf', 'Attached PDF');
+
+        $this->assertCount(2, $pdfDOcumentBuilder->getaddAdditionalDocument());
+        $this->assertArrayHasKey(0, $pdfDOcumentBuilder->getaddAdditionalDocument());
+        $this->assertArrayHasKey(1, $pdfDOcumentBuilder->getaddAdditionalDocument());
+        $this->assertIsArray($pdfDOcumentBuilder->getaddAdditionalDocument()[0]);
+        $this->assertIsArray($pdfDOcumentBuilder->getaddAdditionalDocument()[1]);
+        $this->assertArrayHasKey('content', $pdfDOcumentBuilder->getaddAdditionalDocument()[0]);
+        $this->assertArrayHasKey('filename', $pdfDOcumentBuilder->getaddAdditionalDocument()[0]);
+        $this->assertArrayHasKey('displayname', $pdfDOcumentBuilder->getaddAdditionalDocument()[0]);
+        $this->assertArrayHasKey('relationship', $pdfDOcumentBuilder->getaddAdditionalDocument()[0]);
+        $this->assertArrayHasKey('mimetype', $pdfDOcumentBuilder->getaddAdditionalDocument()[0]);
+        $this->assertNotEquals('', $pdfDOcumentBuilder->getaddAdditionalDocument()[0]['content']);
+        $this->assertSame('02_picture.jpg', $pdfDOcumentBuilder->getaddAdditionalDocument()[0]['filename']);
+        $this->assertSame('02_picture.jpg', $pdfDOcumentBuilder->getaddAdditionalDocument()[0]['displayname']);
+        $this->assertSame('Supplement', $pdfDOcumentBuilder->getaddAdditionalDocument()[0]['relationship']);
+        $this->assertSame('image/jpeg', $pdfDOcumentBuilder->getaddAdditionalDocument()[0]['mimetype']);
+        $this->assertArrayHasKey('content', $pdfDOcumentBuilder->getaddAdditionalDocument()[1]);
+        $this->assertArrayHasKey('filename', $pdfDOcumentBuilder->getaddAdditionalDocument()[1]);
+        $this->assertArrayHasKey('displayname', $pdfDOcumentBuilder->getaddAdditionalDocument()[1]);
+        $this->assertArrayHasKey('relationship', $pdfDOcumentBuilder->getaddAdditionalDocument()[1]);
+        $this->assertArrayHasKey('mimetype', $pdfDOcumentBuilder->getaddAdditionalDocument()[1]);
+        $this->assertNotEquals('', $pdfDOcumentBuilder->getaddAdditionalDocument()[1]['content']);
+        $this->assertSame('pdf.pdf', $pdfDOcumentBuilder->getaddAdditionalDocument()[1]['filename']);
+        $this->assertSame('Attached PDF', $pdfDOcumentBuilder->getaddAdditionalDocument()[1]['displayname']);
+        $this->assertSame('Supplement', $pdfDOcumentBuilder->getaddAdditionalDocument()[1]['relationship']);
+        $this->assertSame('application/pdf', $pdfDOcumentBuilder->getaddAdditionalDocument()[1]['mimetype']);
 
         // PdfConstrucor Getter/Setter
 
@@ -224,5 +275,53 @@ final class InvoiceSuitePdfDocumentBuilderTest extends TestCase
         $this->assertSame("Some author template", $propPdfConstructorValue->getMetaInformationAuthorTemplate());
         $pdfDOcumentBuilder->setMetaInformationAuthorTemplate('My author template');
         $this->assertSame("My author template", $propPdfConstructorValue->getMetaInformationAuthorTemplate());
+
+        $this->assertSame("Some keyword template", $propPdfConstructorValue->getMetaInformationKeywordTemplate());
+        $pdfDOcumentBuilder->setMetaInformationKeywordTemplate('My keyword template');
+        $this->assertSame("My keyword template", $propPdfConstructorValue->getMetaInformationKeywordTemplate());
+
+        $this->assertSame("Some title template", $propPdfConstructorValue->getMetaInformationTitleTemplate());
+        $pdfDOcumentBuilder->setMetaInformationTitleTemplate('My title template');
+        $this->assertSame("My title template", $propPdfConstructorValue->getMetaInformationTitleTemplate());
+
+        $this->assertSame("Some subject template", $propPdfConstructorValue->getMetaInformationSubjectTemplate());
+        $pdfDOcumentBuilder->setMetaInformationSubjectTemplate('My subject template');
+        $this->assertSame("My subject template", $propPdfConstructorValue->getMetaInformationSubjectTemplate());
+
+        $this->assertInstanceOf(Closure::class, $propPdfConstructorValue->getMetaInformationCallback());
+        $pdfDOcumentBuilder->setMetaInformationCallback(null);
+        $this->assertNull($propPdfConstructorValue->getMetaInformationCallback());
+
+        $this->assertTrue($propPdfConstructorValue->getAttachmentPaneVisibility());
+        $pdfDOcumentBuilder->setAttachmentPaneVisibility(false);
+        $this->assertFalse($propPdfConstructorValue->getAttachmentPaneVisibility());
+        $pdfDOcumentBuilder->setAttachmentPaneVisibility(true);
+        $this->assertTrue($propPdfConstructorValue->getAttachmentPaneVisibility());
+
+        $this->assertCount(2, $propPdfConstructorValue->getaddAdditionalDocuments());
+        $this->assertArrayHasKey(0, $propPdfConstructorValue->getaddAdditionalDocuments());
+        $this->assertArrayHasKey(1, $propPdfConstructorValue->getaddAdditionalDocuments());
+        $this->assertIsArray($propPdfConstructorValue->getaddAdditionalDocuments()[0]);
+        $this->assertIsArray($propPdfConstructorValue->getaddAdditionalDocuments()[1]);
+        $this->assertArrayHasKey('content', $propPdfConstructorValue->getaddAdditionalDocuments()[0]);
+        $this->assertArrayHasKey('filename', $propPdfConstructorValue->getaddAdditionalDocuments()[0]);
+        $this->assertArrayHasKey('displayname', $propPdfConstructorValue->getaddAdditionalDocuments()[0]);
+        $this->assertArrayHasKey('relationship', $propPdfConstructorValue->getaddAdditionalDocuments()[0]);
+        $this->assertArrayHasKey('mimetype', $propPdfConstructorValue->getaddAdditionalDocuments()[0]);
+        $this->assertNotEquals('', $propPdfConstructorValue->getaddAdditionalDocuments()[0]['content']);
+        $this->assertSame('02_picture.jpg', $propPdfConstructorValue->getaddAdditionalDocuments()[0]['filename']);
+        $this->assertSame('02_picture.jpg', $propPdfConstructorValue->getaddAdditionalDocuments()[0]['displayname']);
+        $this->assertSame('Supplement', $propPdfConstructorValue->getaddAdditionalDocuments()[0]['relationship']);
+        $this->assertSame('image/jpeg', $propPdfConstructorValue->getaddAdditionalDocuments()[0]['mimetype']);
+        $this->assertArrayHasKey('content', $propPdfConstructorValue->getaddAdditionalDocuments()[1]);
+        $this->assertArrayHasKey('filename', $propPdfConstructorValue->getaddAdditionalDocuments()[1]);
+        $this->assertArrayHasKey('displayname', $propPdfConstructorValue->getaddAdditionalDocuments()[1]);
+        $this->assertArrayHasKey('relationship', $propPdfConstructorValue->getaddAdditionalDocuments()[1]);
+        $this->assertArrayHasKey('mimetype', $propPdfConstructorValue->getaddAdditionalDocuments()[1]);
+        $this->assertNotEquals('', $propPdfConstructorValue->getaddAdditionalDocuments()[1]['content']);
+        $this->assertSame('pdf.pdf', $propPdfConstructorValue->getaddAdditionalDocuments()[1]['filename']);
+        $this->assertSame('Attached PDF', $propPdfConstructorValue->getaddAdditionalDocuments()[1]['displayname']);
+        $this->assertSame('Supplement', $propPdfConstructorValue->getaddAdditionalDocuments()[1]['relationship']);
+        $this->assertSame('application/pdf', $propPdfConstructorValue->getaddAdditionalDocuments()[1]['mimetype']);
     }
 }
