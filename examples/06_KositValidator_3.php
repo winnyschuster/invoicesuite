c104a200cc5e54f64d14a329d536dfefb6c82499
HorstOeko <horstoeko@erling.com.de>
2025-12-29 22:11:31 +0100

    [ENH] Added new Example for KOSIT-Validator

---
+++ examples/06_KositValidator_3.php
@@ -0,0 +1,26 @@
+<?php
+
+use horstoeko\invoicesuite\utils\InvoiceSuitePathUtils;
+use horstoeko\invoicesuite\validators\kosit\InvoiceSuiteKositDocumentValidator;
+
+require __DIR__ . "/../vendor/autoload.php";
+
+// Create (Remote-) Validator
+
+$contents = file_get_contents(InvoiceSuitePathUtils::combinePathWithFile(__DIR__, "/../tests/assets/00_case_xrechnung_cii_simple.xml"));
+
+$validator = InvoiceSuiteKositDocumentValidator::createFromContent($contents);
+$validator->enableRemoteMode();
+$validator->setRemoteModeHost('192.168.1.83');
+$validator->setRemoteModePort(8081);
+$validator->validate();
+
+echo "Finished\n";
+echo sprintf("HasErrorMessages .......... %s\n", $validator->hasErrorMessagesInMessageBag());
+echo sprintf("HasWarningMessages ........ %s\n", $validator->hasWarningMessagesInMessageBag());
+echo sprintf("HasLogMessages ............ %s\n", $validator->hasInfoMessagesInMessageBag());
+
+foreach ($validator->getErrorMessagesInMessageBag() as $message)
+{
+    echo sprintf("  ....... %s\n", $message->getMessageContent());
+}
