7b38118c290a520740bf0e74348cb17743bd5b7c
HorstOeko <horstoeko@erling.com.de>
2026-01-22 05:36:06 +0100

    [ENH] FatturaPA Changed Root Namespace

--- src/documents/providers/fatturapa/models/FatturaElettronica.php
+++ src/documents/providers/fatturapa/models/FatturaElettronica.php
@@ -8,7 +8,7 @@ use horstoeko\invoicesuite\concerns\HandlesObjectFlags;
 use JMS\Serializer\Annotation as JMS;
 
 /**
- * @JMS\XmlRoot("FatturaElettronica", namespace="http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2")
+ * @JMS\XmlRoot("p:FatturaElettronica", namespace="http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2")
  * @JMS\XmlNamespace(uri="http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2", prefix="p")
  * @JMS\XmlNamespace(uri="http://www.w3.org/2000/09/xmldsig#", prefix="ds")
  * @JMS\XmlNamespace(uri="http://www.w3.org/2001/XMLSchema-instance", prefix="xsi")
