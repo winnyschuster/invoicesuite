9786262f4d7b6aa3fcff99deca85650c8a5eebfa
HorstOeko <horstoeko@erling.com.de>
2026-01-24 11:01:31 +0100

    [DOC] Fixed file docs (Zugferd, Compat)

--- src/compat/zugferd/codelists/ZugferdChargeCodes.php
+++ src/compat/zugferd/codelists/ZugferdChargeCodes.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is a part of horstoeko/zugferd.
+ * This file is a part of horstoeko/invoicesuite.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
