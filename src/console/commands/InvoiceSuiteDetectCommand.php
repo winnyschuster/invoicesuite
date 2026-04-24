3c877e1339a67e1878740a4343472557c08c95ee
HorstOeko <horstoeko@erling.com.de>
2026-04-24 05:00:40 +0200

    [ENH] Improved InvoiceSuiteDetectCommand

--- src/console/commands/InvoiceSuiteDetectCommand.php
+++ src/console/commands/InvoiceSuiteDetectCommand.php
@@ -14,14 +14,18 @@ namespace horstoeko\invoicesuite\console\commands;
 use horstoeko\invoicesuite\exceptions\InvoiceSuiteFileNotFoundException;
 use horstoeko\invoicesuite\exceptions\InvoiceSuiteFileNotReadableException;
 use horstoeko\invoicesuite\exceptions\InvoiceSuiteFormatProviderNotFoundException;
+use horstoeko\invoicesuite\exceptions\InvoiceSuiteInvalidArgumentException;
 use horstoeko\invoicesuite\exceptions\InvoiceSuiteUnknownContentException;
 use horstoeko\invoicesuite\InvoiceSuiteDocumentReader;
 use horstoeko\invoicesuite\InvoiceSuitePdfDocumentReader;
+use horstoeko\invoicesuite\utils\InvoiceSuiteArrayUtils;
 use PrinsFrank\PdfParser\Exception\PdfParserException;
 use RuntimeException;
 use Symfony\Component\Console\Exception\InvalidArgumentException;
+use Symfony\Component\Console\Exception\InvalidArgumentException as ConsoleInvalidArgumentException;
 use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  * Class representing a console command that detects the format of a given file
@@ -45,6 +49,7 @@ class InvoiceSuiteDetectCommand extends InvoiceSuiteAbstractCommand
         $this->setName('invoicesuite:detect');
         $this->setDescription('Detect the format of the given file');
         $this->addArgument('input-file', InputArgument::REQUIRED, 'The file to detect the format of');
+        $this->addOption('outputtype', 'ot', InputOption::VALUE_REQUIRED, 'Output format (screen, json)');
     }
 
     /**
@@ -56,22 +61,28 @@ class InvoiceSuiteDetectCommand extends InvoiceSuiteAbstractCommand
      * @throws InvoiceSuiteFileNotFoundException
      * @throws InvoiceSuiteFileNotReadableException
      * @throws InvoiceSuiteFormatProviderNotFoundException
+     * @throws InvoiceSuiteInvalidArgumentException
      * @throws InvoiceSuiteUnknownContentException
      * @throws PdfParserException
      * @throws RuntimeException
      */
     protected function handle(): int
     {
-        $argFilename = $this->getFileArgument('input-file');
+        $inpArgFilename = $this->getFileArgument('input-file');
+        $inpOptionFormat = $this->getStringOption('outputtype', 'screen');
 
-        if ($this->isPdfFilename($argFilename)) {
-            $pdfReader = InvoiceSuitePdfDocumentReader::createFromFile($argFilename);
+        if (!InvoiceSuiteArrayUtils::inArrayNoCase(['screen', 'json'], $inpOptionFormat)) {
+            throw new InvoiceSuiteInvalidArgumentException(sprintf('Invalid option value for output type "%s"', $inpOptionFormat));
+        }
+
+        if ($this->isPdfFilename($inpArgFilename)) {
+            $pdfReader = InvoiceSuitePdfDocumentReader::createFromFile($inpArgFilename);
 
             return $this->handlePdf($pdfReader)->returnSuccess();
         }
 
-        if ($this->isXmlFilename($argFilename) || $this->isJsonFilename($argFilename)) {
-            $xmlOrJsonReader = InvoiceSuiteDocumentReader::createFromFile($argFilename);
+        if ($this->isXmlFilename($inpArgFilename) || $this->isJsonFilename($inpArgFilename)) {
+            $xmlOrJsonReader = InvoiceSuiteDocumentReader::createFromFile($inpArgFilename);
 
             return $this->handleXml($xmlOrJsonReader)->returnSuccess();
         }
@@ -109,9 +120,19 @@ class InvoiceSuiteDetectCommand extends InvoiceSuiteAbstractCommand
      *
      * @param  InvoiceSuiteDocumentReader $xmlOrJsonReader
      * @return static
+     *
+     * @throws ConsoleInvalidArgumentException
+     * @throws RuntimeException
      */
     protected function handleXml(InvoiceSuiteDocumentReader $xmlOrJsonReader): static
     {
+        if (0 === strcasecmp($this->getStringOption('outputtype', 'screen'), 'json')) {
+            $json['id'] = $xmlOrJsonReader->getCurrentDocumentFormatProvider()->getUniqueId();
+            $json['description'] = $xmlOrJsonReader->getCurrentDocumentFormatProvider()->getDescription();
+
+            return $this->outputLineLF(json_encode($json));
+        }
+
         $tableRows[] = ['ID', $xmlOrJsonReader->getCurrentDocumentFormatProvider()->getUniqueId()];
         $tableRows[] = ['Description', mb_strimwidth($xmlOrJsonReader->getCurrentDocumentFormatProvider()->getDescription(), 0, 60, '...')];
 
